262e2547573214506516d9463ea177f051d03619
JavierGMESA <javier.garmesa@alum.uca.es>
2024-10-28 12:17:06 +0100

    a

--- src/P4/P4.java
+++ src/P4/P4.java
@@ -1,7 +1,5 @@
 package P4;
 
-import java.lang.Thread;
-
 public class P4 {
     private volatile int n = 0; // IMPORTANTE: Las lecturas y escrituras de la variable se hacen en MP, no en
                                 // caché (toda modificación se refleja al instante, pues cada procesador no mira
