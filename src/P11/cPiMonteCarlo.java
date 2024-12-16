9ca5b8a4ee11330fba11538abb7e0df797dc6b4b
JavierGMESA <javier.garmesa@alum.uca.es>
2024-12-16 11:59:30 +0100

    Práctica 11 terminada

--- src/P11/cPiMonteCarlo.java
+++ src/P11/cPiMonteCarlo.java
@@ -1,6 +1,55 @@
 package P11;
 
+import java.net.MalformedURLException;
+import java.rmi.*;
+import java.rmi.registry.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
 public class cPiMonteCarlo 
 {
-    
+    public static void main(String[] args)
+    {
+        String URLRegistro = "rmi://localhost:8080/bonoloto";
+        try
+        {
+            iPiMonteCarlo objeto = (iPiMonteCarlo) Naming.lookup(URLRegistro);
+            objeto.reset();
+            ExecutorService pool = Executors.newFixedThreadPool(10);
+            for(int i = 0; i < 9; ++i) pool.submit(new MonteCarloHebra(objeto));
+            pool.shutdown();
+            pool.awaitTermination(10, TimeUnit.DAYS);
+            
+
+            //objeto.masPuntos(10000);
+            System.out.println("La aproximación da " + objeto.aproxActual());
+        }catch(Exception e)
+        {
+            System.out.println("Se ha producido una excepción a la hora de buscar el servidor");
+            e.getMessage();
+        }
+    }
+}
+
+class MonteCarloHebra implements Runnable
+{
+    iPiMonteCarlo objeto;
+    public MonteCarloHebra(iPiMonteCarlo objeto)
+    {
+        this.objeto = objeto;
+    }
+
+    public void run()
+    {
+        try
+        {
+            for(int i = 0; i < 10; ++i) objeto.masPuntos(100);
+            System.out.println("Hilo terminado");
+        }catch(Exception e)
+        {
+            e.getMessage();
+        }
+    }
 }
--- src/P11/sPiMonteCarlo.java
+++ src/P11/sPiMonteCarlo.java
@@ -4,8 +4,66 @@ import java.net.MalformedURLException;
 import java.rmi.*;
 import java.rmi.registry.*;
 import java.rmi.server.*;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class sPiMonteCarlo extends UnicastRemoteObject implements iPiMonteCarlo
 {
+    private int totalPuntos;
+    private int puntosDentroDelCirculo;
+
+    public sPiMonteCarlo() throws RemoteException
+    {
+        totalPuntos = puntosDentroDelCirculo = 0;
+    }
+
+    public synchronized void reset() throws RemoteException         //IMPORTANTE: HAY QUE HACERLOS synchronized PARA QUE NO OCURRAN CONDICIONES DE CARRERA
+    {
+        totalPuntos = puntosDentroDelCirculo = 0;
+    }
+    public synchronized void masPuntos(int nPuntos)  throws RemoteException
+    {
+        totalPuntos += nPuntos;
+        for(int i = 0; i < nPuntos; ++i)
+        {
+            double x = ThreadLocalRandom.current().nextDouble();
+            double y = ThreadLocalRandom.current().nextDouble();
+            if(x * x + y * y <= 1)
+            {
+                puntosDentroDelCirculo++;
+            }
+        }
+    }
+    public synchronized double aproxActual() throws RemoteException
+    {
+        return 4.0 * puntosDentroDelCirculo / totalPuntos;
+    }
+
+    private static void arrancarRegistro(int numPuerto) throws RemoteException
+    {
+        try
+        {
+            Registry res = LocateRegistry.getRegistry(numPuerto);
+            res.list();
+        }catch(Exception e)
+        {
+            Registry res = LocateRegistry.createRegistry(numPuerto);
+        }
+    }
     
+    public static void main(String[] args) throws RemoteException
+    {
+        sPiMonteCarlo obj = new sPiMonteCarlo();
+        int NumPuerto = 8080;
+        arrancarRegistro(NumPuerto);
+        String URLRegistro = "rmi://localhost:" + NumPuerto + "/bonoloto";
+        try
+        {
+            Naming.rebind(URLRegistro, obj);
+        }catch(MalformedURLException e)
+        {
+            System.out.println("Error con la url");
+        }
+        System.out.println("Servidor preparado");
+    }
 }
